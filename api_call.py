f2ad9b598af7e0b0336ea7c8f166ab0db99fe7af
stephane-cloudflare <stephane@cloudflare.com>
2017-03-02 18:01:36 -0800

    try to make http call

--- api_call.py
+++ api_call.py
@@ -1,4 +1,12 @@
-print("Goodbye, World!")
 
-#Commentaire
-#nouveau Commentaire
+url = 'https://www.justalittlebyte.ovh'
+
+import urllib2
+
+#opener = urllib2.build_opener()
+#opener.addheaders = [('host', 'www.justalittlebyte.ovh')]
+#opener.open(url)
+
+req = urllib2.Request(url, '',)
+req.add_header = ('User-agent', 'Mozilla/5.0'), ('host', 'www.justalittlebyte.ovh') , ('X-CF-Trace', '1')
+resp = urllib2.urlopen(req)
